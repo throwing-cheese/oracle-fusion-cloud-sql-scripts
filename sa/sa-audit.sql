287ccf8836b1a9d5ae9bb8a7e215088e7e7fac56
throwing-cheese <143490562+throwing-cheese@users.noreply.github.com>
2023-12-19 13:44:41 +0000

    Update sa-audit.sql

--- sa/sa-audit.sql
+++ sa/sa-audit.sql
@@ -1,8 +1,5 @@
 /*
 File Name: ap-audit.sql
-Version: Oracle Fusion Cloud
-Author: Throwing Cheese
-URL: https://github.com/throwing-cheese/oracle-fusion-cloud-sql-scripts
 */
 
 /*
@@ -10,10 +7,25 @@ FND_AUDIT_ATTRIBUTES
 This table will store the attributes enabled for Auditing.
 */
 
-select *
-from FND_AUDIT_ATTRIBUTES
-where table_name = 'AP_INVOICES_ALL'
+		select *
+		  from fnd_audit_attributes
+		 where table_name = 'AP_INVOICES_ALL'
 
-select *
-from FND_AUDIT_ATTRIBUTES
-where table_name like 'AP_INVOICE%'
+		select *
+		  from fnd_audit_attributes
+		 where table_name like 'AP_INVOICE%'
+
+		select table_name
+			 , enabled_flag
+			 , created_by
+			 , min(to_char(creation_date, 'yyyy-mm-dd hh24:mi:ss')) min_created
+			 , max(to_char(creation_date, 'yyyy-mm-dd hh24:mi:ss')) max_created
+			 , min(view_attribute) min_view_attribute
+			 , max(view_attribute) max_view_attribute
+			 , min(column_name) min_column_name
+			 , max(column_name) max_column_name
+			 , count(*)
+		  from fnd_audit_attributes
+	  group by table_name
+			 , enabled_flag
+			 , created_by
